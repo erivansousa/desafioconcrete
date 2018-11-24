b95742e6f11334f50f4e64206fb1211d9bb403ae
erivan <erivan.spe@gmail.com>
2018-11-24 11:27:39 -0300

    iniciando o projeto

---
+++ src/main/java/com/desafio/concrete/DesafioConcreteApplication.java
@@ -0,0 +1,12 @@
+package com.desafio.concrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioConcreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioConcreteApplication.class, args);
+	}
+}
